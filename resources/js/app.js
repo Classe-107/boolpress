0d7c33677cbd6090fea873ca483b0de3ed252e2a
klelia <clelia.fradella@gmail.com>
2024-01-15 18:53:32 +0100

    added bs-admin template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,44 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
+import Chart from "chart.js";
+import { DataTable } from "simple-datatables";
+import "./chart-area-demo.js";
+import "./chart-bar-demo.js";
+import "./chart-area-demo.js";
+import "./chart-area-demo.js";
+
 import.meta.glob(["../img/**", "../fonts/**"]);
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector("#sidebarToggle");
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener("click", (event) => {
+            event.preventDefault();
+            document.body.classList.toggle("sb-sidenav-toggled");
+            localStorage.setItem(
+                "sb|sidebar-toggle",
+                document.body.classList.contains("sb-sidenav-toggled")
+            );
+        });
+    }
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById("datatablesSimple");
+    if (datatablesSimple) {
+        const dataTable = new DataTable(datatablesSimple);
+        //new simpleDatatables.DataTable(datatablesSimple);//
+    }
+});
 
 const deleteSubmitButtons = document.querySelectorAll(".delete-button");
 
