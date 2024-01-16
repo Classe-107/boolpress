643b9ad3a255dcee0953a54569814d30bc458e4b
klelia <clelia.fradella@gmail.com>
2024-01-16 13:46:36 +0100

    added menu for categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,15 +2,13 @@
 @section('content')
     <section class="container">
         <h1>{{$category->name}}</h1>
+        <h3>Post List</h3>
         <ul>
-@forelse ($category->posts as $post)
-    <li> {{$post->title}}</li>
-@empty
-    <li>No posts</li>
-@endforelse
-
-
+            @forelse ($category->posts as $post)
+                <li> {{$post->title}}</li>
+            @empty
+                <li>No posts</li>
+            @endforelse
         </ul>
-
     </section>
 @endsection
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,15 +8,26 @@
                                 Dashboard
                             </a>
                             <div class="sb-sidenav-menu-heading">Resources</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePosts" aria-expanded="false" aria-controls="collapsePosts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Posts
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapsePosts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="{{route('admin.posts.index')}}">All Posts</a>
                                     <a class="nav-link" href="{{route('admin.posts.create')}}">Create new post</a>
+                                </nav>
+                            </div>
+                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategories" aria-expanded="false" aria-controls="collapseCategories">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Categories
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseCategories" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="{{route('admin.categories.index')}}">All Categories</a>
+                                    <a class="nav-link" href="{{route('admin.categories.create')}}">Create new category</a>
                                 </nav>
                             </div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
