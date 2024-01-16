05a8a7b0faac43a546c55188b083a403f5d4b349
klelia <clelia.fradella@gmail.com>
2024-01-16 16:33:04 +0100

    style fix

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.admin')
 @section('content')
-    <section class="container">
-        <h1>{{$post->title}}</h1>
-        <p>{{ $post->body}}</p>
-        <span>{{$post->category ? $post->category->name : 'Uncategorized'}}</span>
-        	{{-- $post->category?->name --}}
+    <section class="container my-3" id="item-post">
+        <div class="d-flex justify-content-between align-items-center">
+             <h1>{{$post->title}}</h1>
+             <a href="{{route('admin.posts.edit', $post->slug)}}" class="btn btn-success px-3">Edit</a>
+        </div>
+        <div>
+        <p>{!! $post->body !!}</p>
+                @if($post->category)
+                <div class="mb-3">
+                    <h4>Category</h4>
+                    <a class="badge text-bg-primary" href="{{route('admin.categories.show', $post->category->slug)}}">{{$post->category->name}}</a>
+                </div>
+                <img src="{{asset('storage/' . $post->image)}}" alt="{{$post->title}}">
+        </div>
+
+        @endif
     </section>
 @endsection
